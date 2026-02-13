5d09cf2e5d27b35fcc60c24bdc86e8ef91134c2c
Aishwarya <aishushan02@gmail.com>
2026-02-13 12:33:28 +0530

    download resume feature 10

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,5 @@
 import illustration from './illustrationimg.png';
-import resumePdf from 'C:\\Users\\aishu\\Portfolio\\src\\components\\Aishwarya_Resume.pdf';
+import resumePdf from './Aishwarya_Resume.pdf';
 
 export default function Hero() {
   return (
